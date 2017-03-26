73833b78024cf7bc1e9b2155572b6e89313770be
Brian Yu <brian@byu.io>
2017-03-26 07:48:59 -0500

    Fix alex screw up, move vr files

--- server/app.py
+++ server/app.py
@@ -1,8 +1,15 @@
 from flask import Flask, request, render_template
+import os
+
+app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def home():
 	return render_template('index.html')
+
+@app.route('/vr', methods=['GET'])
+def vr():
+	return render_template('vr.html')
 
 if __name__ == '__main__':
 	app.run(port=os.getenv('PORT', 5000), host='0.0.0.0', debug=True)
